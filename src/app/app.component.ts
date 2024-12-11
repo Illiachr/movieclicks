50f4b259f3f5c9a5bca5b7920eb96d1bdcb70c41
Illiachr <74302872+Illiachr@users.noreply.github.com>
2024-12-11 21:52:06 +0100

    feat: mvclk-0001-header (#1)
    added common styles
    
    implemented bootstrap for home page view
    
    implemented header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { AppHeader } from "./components/header/header.component";
+import { HomeView } from "./views/home/home.view";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [AppHeader, HomeView],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'movieclicks';
+  title = "movieclicks";
 }
---
+++ src/app/views/home/home.view.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: "app-home",
+  imports: [RouterOutlet],
+  templateUrl: "./home.view.html",
+  styleUrl: "./home.view.css",
+})
+export class HomeView {}
