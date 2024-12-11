69beb5f9643777502d8ba5b3d4d3c94e13fa83f5
Illia <ilja.cherkasov@gmail.com>
2024-12-11 23:13:03 +0100

    feat: main content
    devloped card component
    
    updated home view

--- src/app/views/home/home.view.ts
+++ src/app/views/home/home.view.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: "app-home",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: "./home.view.html",
   styleUrl: "./home.view.css",
 })
